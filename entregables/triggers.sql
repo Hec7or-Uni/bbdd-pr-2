1cb3fd1d04a270ca6d9755df3e3e78aeacb78651
Hector Toral <hectortoraltrabajos@gmail.com>
2022-04-16 19:30:58 +0200

    Update triggers.sql

--- entregables/triggers.sql
+++ entregables/triggers.sql
@@ -1,5 +1,10 @@
 -- TRIGGER 1:
+-- Al insertar en peliculas verifica que el identificado de la peli X no 
+-- este en la tabla de series
 
 -- TRIGGER 2:
+-- Al insertar en interpreta:
+-- Se inserta en: [personal, participa y colabora]
 
--- TRIGGER 3
+-- TRIGGER 3:
+
