57bcdf4a75a7c3e261e5a1644a0d45288aca5d7d
hec7orci7o <hectortoraltrabajos@gmail.com>
2022-04-20 22:15:40 +0200

    tablas

--- entregables/tablas.sql
+++ entregables/tablas.sql
@@ -1,30 +1,40 @@
 CREATE TABLE generos (
-  nombre  VARCHAR2(100), 
-  CONSTRAINT pk_Gen_nombre  PRIMARY KEY (nombre)
+  nombre        VARCHAR2(100),
+  id_contenido  NUMBER,
+  CONSTRAINT pk_Gen PRIMARY KEY (nombre, id_contenido),
+  CONSTRAINT fk_Gen FOREIGN KEY (id_contenido) REFERENCES contenido(id),
 );
 
 CREATE TABLE contenido (
   id      NUMBER,
   titulo  VARCHAR2(150) NOT NULL,
-  CONSTRAINT pk_Con_id PRIMARY KEY (id)
+  estreno NUMBER        NOT NULL,
+  CONSTRAINT pk_Con_id  PRIMARY KEY (id),
+  CONSTRAINT ck_Con_estreno CHECK (estreno >= 1850)
 );
 
 CREATE TABLE peliculas (
-  id      NUMBER,
-  estreno NUMBER  NOT NULL,
-  CONSTRAINT pk_Pel_id PRIMARY KEY (id),
-  CONSTRAINT fk_Pel_id FOREIGN KEY (id) REFERENCES contenido(id),
-  CONSTRAINT ck_Pel_estreno CHECK (estreno >= 1850)
+  id  NUMBER,
+  CONSTRAINT pk_Pel_id  PRIMARY KEY (id),
+  CONSTRAINT fk_Pel_id  FOREIGN KEY (id) REFERENCES contenido(id)
+);
+
+-- Tiene N:M
+CREATE TABLE relacion_peliculas (
+  id_pelicula_a NUMBER,
+  id_pelicula_b NUMBER,
+  tipo          VARCHAR2(50)  NOT NULL,
+  CONSTRAINT pk_RP    PRIMARY KEY (id_pelicula_a, id_pelicula_b),
+  CONSTRAINT fk_RP_a  FOREIGN KEY (id_pelicula_a) REFERENCES peliculas(id),
+  CONSTRAINT fk_RP_b  FOREIGN KEY (id_pelicula_b) REFERENCES peliculas(id)
 );
 
 CREATE TABLE series (
   id      NUMBER, 
-  estreno NUMBER  NOT NULL,
   fin     NUMBER,
-  CONSTRAINT pk_Ser_id PRIMARY KEY (id),
-  CONSTRAINT fk_Ser_id FOREIGN KEY (id) REFERENCES contenido(id),
-  CONSTRAINT ck_Ser_estreno CHECK (estreno >= 1850),
-  CONSTRAINT ck_Ser_fin CHECK (estreno <= fin)
+  CONSTRAINT pk_Ser_id  PRIMARY KEY (id),
+  CONSTRAINT fk_Ser_id  FOREIGN KEY (id)  REFERENCES contenido(id),
+  CONSTRAINT ck_Ser_fin CHECK (fin >= 1850)
 );
 
 CREATE TABLE capitulos (
@@ -33,17 +43,23 @@ CREATE TABLE capitulos (
   estreno   NUMBER  NOT NULL,
   temporada NUMBER,
   episodio  NUMBER,
-  CONSTRAINT pk_Cap_id PRIMARY KEY (id, nombre),
-  CONSTRAINT fk_Cap_id FOREIGN KEY (id) REFERENCES contenido(id),
+  CONSTRAINT pk_Cap_id  PRIMARY KEY (id, nombre),
+  CONSTRAINT fk_Cap_id  FOREIGN KEY (id)  REFERENCES contenido(id),
   CONSTRAINT ck_Cap_estreno   CHECK (estreno >= 1850),
   CONSTRAINT ck_Cap_temporada CHECK (temporada >= 1),
   CONSTRAINT ck_Cap_episodio  CHECK (episodio >= 1)
 );
 
 CREATE TABLE personal (
-  id        NUMBER, 
+  id        NUMBER,
   nombre    VARCHAR2(100) NOT NULL,
+  genero    VARCHAR2(100),
   CONSTRAINT pk_P_id PRIMARY KEY (id)
+);
+
+CREATE TABLE actores (
+  id  NUMBER,
+  CONSTRAINT pk_Act PRIMARY KEY (id)
 );
 
 CREATE TABLE personajes (
@@ -53,59 +69,34 @@ CREATE TABLE personajes (
   CONSTRAINT pk_Per_id PRIMARY KEY (id)
 );
 
--- Tiene N:M
-CREATE TABLE contenido_tiene_genero (
-  id_contenido  NUMBER,
-  nombre        VARCHAR2(100),
-  CONSTRAINT pk_CTG PRIMARY KEY (id_contenido, nombre),
-  CONSTRAINT fk_CTG_id_contenido FOREIGN KEY (id_contenido) REFERENCES contenido(id),
-  CONSTRAINT fk_CTG_nombre       FOREIGN KEY (nombre)       REFERENCES generos(nombre)
+CREATE TABLE roles (
+  rol VARCHAR2(100),
+  CONSTRAINT pk_Rol PRIMARY KEY (rol)
 );
 
--- Tiene N:M
-CREATE TABLE relacion_peliculas (
-  id_pelicula_a NUMBER,
-  id_pelicula_B NUMBER,
-  tipo          VARCHAR2(50)  NOT NULL,
-  CONSTRAINT pk_RP    PRIMARY KEY (id_pelicula_a, id_pelicula_B),
-  CONSTRAINT fk_RP_a  FOREIGN KEY (id_pelicula_a) REFERENCES peliculas(id),
-  CONSTRAINT fk_RP_b  FOREIGN KEY (id_pelicula_B) REFERENCES peliculas(id)
-);
-
--- Tiene N:M
 CREATE TABLE colabora (
   id_personal   NUMBER,
   id_contenido  NUMBER,
-  rol           VARCHAR2(100)  NOT NULL,
-  CONSTRAINT pk_Col PRIMARY KEY (id_personal, id_contenido),
-  CONSTRAINT fk_Col_personal  FOREIGN KEY (id_personal)  REFERENCES personal(id),
-  CONSTRAINT fk_Col_contenido FOREIGN KEY (id_contenido) REFERENCES contenido(id)
+  rol           VARCHAR2(100),
+  CONSTRAINT pk_Col           PRIMARY KEY (id_personal, id_contenido, rol),
+  CONSTRAINT fk_Col_personal  FOREIGN KEY (id_personal)   REFERENCES personal(id),
+  CONSTRAINT fk_Col_contenido FOREIGN KEY (id_contenido)  REFERENCES contenido(id),
+  CONSTRAINT fk_Col_rol       FOREIGN KEY (rol)           REFERENCES contenido(id)
 );
 
--- Tiene N:M
 CREATE TABLE participa (
-  id_contenido NUMBER,
-  id_personaje NUMBER,
-  CONSTRAINT pk_Par PRIMARY KEY (id_contenido, id_personaje),
-  CONSTRAINT fk_Par_contenido FOREIGN KEY (id_contenido) REFERENCES contenido(id),
-  CONSTRAINT fk_Par_personaje FOREIGN KEY (id_personaje) REFERENCES personajes(id)
+  id_actor      NUMBER,
+  id_contenido  NUMBER,
+  CONSTRAINT pk_Par           PRIMARY KEY (id_contenido, id_actor),
+  CONSTRAINT fk_Par_actor     FOREIGN KEY (id_actor)      REFERENCES actores(id),
+  CONSTRAINT fk_Par_contenido FOREIGN KEY (id_contenido)  REFERENCES contenido(id)
 );
 
--- Tiene N:M
 CREATE TABLE interpreta (
-  id_personal  NUMBER,
-  id_contenido NUMBER,
-  id_personaje NUMBER,
-  CONSTRAINT pk_Int PRIMARY KEY (id_personal, id_contenido, id_personaje),
-  CONSTRAINT fk_Int_personal  FOREIGN KEY (id_personal)                REFERENCES personal(id),
-  CONSTRAINT fk_Int_participa FOREIGN KEY (id_contenido, id_personaje) REFERENCES participa(id_contenido, id_personaje)
-);
-
--- Tiene N:M
-CREATE TABLE dirige (
-  id_personal  NUMBER,
-  id_contenido NUMBER,
-  CONSTRAINT pk_Dir PRIMARY KEY (id_personal, id_contenido),
-  CONSTRAINT fk_Dir_personal  FOREIGN KEY (id_personal)   REFERENCES personal(id),
-  CONSTRAINT fk_Dir_contenido FOREIGN KEY (id_contenido)  REFERENCES contenido(id)
+  id_actor      NUMBER,
+  id_contenido  NUMBER,
+  id_personaje  NUMBER,
+  CONSTRAINT pk_Int           PRIMARY KEY (id_actor, id_contenido, id_personaje),
+  CONSTRAINT fk_Int_personal  FOREIGN KEY (id_actor)                    REFERENCES actores(id),
+  CONSTRAINT fk_Int_participa FOREIGN KEY (id_contenido, id_personaje)  REFERENCES participa(id_contenido, id_personaje)
 );
