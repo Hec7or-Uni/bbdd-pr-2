202d95f302670fd800205134bbf99faaec7c1274
Francisco Javier Pizarro <pizarrojavier000@gmail.com>
2022-04-21 11:51:43 +0200

    Base casi poblada

---
+++ entregables/sqlNuevo/roles.sql
@@ -0,0 +1,16 @@
+/*
+-- Query: select role from role_type
+-- Date: 2022-04-21 08:34
+*/
+INSERT INTO rol (`rol`) VALUES ('actor');
+INSERT INTO rol (`rol`) VALUES ('actress');
+INSERT INTO rol (`rol`) VALUES ('cinematographer');
+INSERT INTO rol (`rol`) VALUES ('composer');
+INSERT INTO rol (`rol`) VALUES ('costume designer');
+INSERT INTO rol (`rol`) VALUES ('director');
+INSERT INTO rol (`rol`) VALUES ('editor');
+INSERT INTO rol (`rol`) VALUES ('guest');
+INSERT INTO rol (`rol`) VALUES ('miscellaneous crew');
+INSERT INTO rol (`rol`) VALUES ('producer');
+INSERT INTO rol (`rol`) VALUES ('production designer');
+INSERT INTO rol (`rol`) VALUES ('writer');
