1d7b29c9726003ad613259f5cca74f4e1cbeb59c
hec7orci7o <hectortoraltrabajos@gmail.com>
2022-04-23 20:43:58 +0200

    inserts

---
+++ entregables/sql/generos.sql
@@ -0,0 +1,38 @@
+/*
+-- Query: 
+  select "terror" as nombre, m.movie_id as id_contenido
+  from movie_keyword m, keyword k
+  where m.keyword_id = k.id and (k.keyword like '%horror%' or k.keyword like '%terror');
+-- Date: 2022-04-23 20:19
+*/
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',1644005);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',1647723);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',1703077);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',1822383);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',1830767);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',1863994);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',1863994);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',1863994);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',1933864);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',1933864);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',1933864);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',1993132);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',1993298);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',1994456);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',1994456);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',2009552);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',2015714);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',2067585);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',2067585);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',2067585);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',2144618);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',2240570);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',2240570);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',2240570);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',2424101);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',2424101);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',2424101);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',2424101);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',2465768);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',2465768);
+INSERT INTO generos (nombre,id_contenido) VALUES ('terror',2466485);
---
+++ entregables/sql/roles.sql
@@ -0,0 +1,18 @@
+/*
+-- Query: 
+  select role as rol
+  from role_type;
+-- Date: 2022-04-23 20:28
+*/
+INSERT INTO roles (rol) VALUES ('actor');
+INSERT INTO roles (rol) VALUES ('actress');
+INSERT INTO roles (rol) VALUES ('cinematographer');
+INSERT INTO roles (rol) VALUES ('composer');
+INSERT INTO roles (rol) VALUES ('costume designer');
+INSERT INTO roles (rol) VALUES ('director');
+INSERT INTO roles (rol) VALUES ('editor');
+INSERT INTO roles (rol) VALUES ('guest');
+INSERT INTO roles (rol) VALUES ('miscellaneous crew');
+INSERT INTO roles (rol) VALUES ('producer');
+INSERT INTO roles (rol) VALUES ('production designer');
+INSERT INTO roles (rol) VALUES ('writer');
