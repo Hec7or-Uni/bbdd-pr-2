ed46d5c973d73e0a4042c1b681edf8733d67d6ac
Francisco Javier Pizarro <pizarrojavier000@gmail.com>
2022-04-26 08:16:22 +0200

    Consultas 1 y 2 fixed

--- entregables/consultas.sql
+++ entregables/consultas.sql
@@ -1,16 +1,47 @@
 --CONSULTA 1
 --Porcentaje de películas con hasta 3 actores o actrices
-SELECT R / T * 100 FROM
-(SELECT COUNT(*) as R FROM contenido C WHERE (SELECT COUNT(*) FROM participa P WHERE C.id = P.id_contenido)  < 4),
-(SELECT COUNT(*) as T FROM contenido C);
+SELECT ROUND(N.num_conteidos_limite_actores / D.num_contenidos * 100, 2) AS porcentaje
+FROM (
+  -- Devuelve cuantos contenidos hay con hasta 3 actores/actrices
+  SELECT COUNT(*) AS num_conteidos_limite_actores
+FROM (
+    -- Devuelve tuplas (num_actores, id_contenido) en las que el num_actores es <= 3 
+    SELECT id_contenido
+    FROM participa
+    GROUP BY id_contenido
+    HAVING COUNT(*) <= 3
+)
+) N, (
+  -- Devuelve el numero total de contenidos
+  SELECT COUNT(*) AS num_contenidos
+  FROM contenido
+) D;
 
 
---CONSULTA2
---Título de las películas y series de terror en las que el mismo personaje 
---ha sido interpretado por al menos 50 diferentes actores o actrices, 
---junto con el nombre del personaje y el número de actores distintos que lo han interpretado
-SELECT DISTINCT C.titulo,P.nombre,(SELECT COUNT(*) FROM interpreta B WHERE A.id_personaje = B.id_personaje AND A.id_contenido = B.id_contenido) 
-FROM interpreta A, generos G, personajes P, contenido C 
-WHERE A.id_contenido = G.id_contenido AND G.nombre = 'terror' AND 
-(SELECT COUNT(*) AS Z FROM interpreta B WHERE A.id_personaje = B.id_personaje AND A.id_contenido = B.id_contenido) > 49 AND 
-P.id = A.id_personaje AND C.id = A.id_contenido;
+-- CONSULTA 2
+-- Título de las películas y series de terror en las que el mismo personaje 
+-- ha sido interpretado por al menos 50 diferentes actores o actrices, 
+-- junto con el nombre del personaje y el número de actores distintos que lo han interpretado
+SELECT N.titulo, P.nombre, N.num_actores
+FROM personajes P, (
+  -- Devuelve tuplas (id, titulo, num_actores, id_personaje) en las que el contenido es de terror y el 
+  -- numero de veces que un personaje es interpretado dentro del mismo contenido es de al menos 50.
+  SELECT A.id, A.titulo, B.num_actores, B.id_personaje
+  FROM (
+    -- Devuelve una lista de tuplas (id, titulo) de aquellos contenidos que
+    -- pertenecen al genero de terror.
+    SELECT C.id, C.titulo
+    FROM contenido C, generos G
+    WHERE C.id = G.id_contenido and G.nombre = 'terror'
+  ) A, (
+    -- Devuelve tuplas como (numero de veces, id_contenido, id_personaje) en las que
+    -- el numero de actores para interpretar un personaje dentro de un mismo contenido (pelicula/serie)
+    -- es al menos de 50
+    SELECT COUNT(*) AS num_actores, id_contenido, id_personaje
+    FROM interpreta I
+    GROUP BY id_contenido, id_personaje
+    HAVING COUNT(*) > 49
+  ) B
+  WHERE A.id = B.id_contenido
+) N
+WHERE P.id = N.id_personaje;
