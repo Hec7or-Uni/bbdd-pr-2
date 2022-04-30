81d2baf21414e8b37c81dfe5a1d9ba9ff50e8e47
hec7orci7o <hectortoraltrabajos@gmail.com>
2022-04-30 18:20:41 +0200

    consultas acabadas

--- entregables/consultas.sql
+++ entregables/consultas.sql
@@ -1,10 +1,10 @@
 --CONSULTA 1
 --Porcentaje de películas con hasta 3 actores o actrices
-SELECT ROUND(N.num_conteidos_limite_actores / D.num_contenidos * 100, 2) AS porcentaje
+SELECT ROUND(N.num_conteidos_limite_actores / D.num_contenidos * 100, 2)||'%' AS porcentaje
 FROM (
   -- Devuelve cuantos contenidos hay con hasta 3 actores/actrices
   SELECT COUNT(*) AS num_conteidos_limite_actores
-FROM (
+  FROM (
     -- Devuelve tuplas (num_actores, id_contenido) en las que el num_actores es <= 3 
     SELECT id_contenido
     FROM participa
@@ -16,7 +16,6 @@ FROM (
   SELECT COUNT(*) AS num_contenidos
   FROM contenido
 ) D;
-
 
 -- CONSULTA 2
 -- Título de las películas y series de terror en las que el mismo personaje 
@@ -45,3 +44,43 @@ FROM personajes P, (
   WHERE A.id = B.id_contenido
 ) N
 WHERE P.id = N.id_personaje;
+
+-- CONSULTA 3
+-- Actores que solo han participado en una película de la década de los 90 
+-- que forma parte de una saga de al menos tres películas
+SELECT nombre
+FROM personal P, (
+    SELECT id_actor
+    FROM participa P , (
+    -- Selecciona de la lista de peliculas que pertenecen a una saga de al menos 3 peliculas, las que
+    -- se estrenaron en la decada de los 90 
+      SELECT DISTINCT S.id
+      FROM contenido C, (
+        -- Saca la lista de sagas con al menos 3 peliculas
+          SELECT id_pelicula_a as id
+          FROM (
+              SELECT *
+              FROM relacion_peliculas
+              WHERE tipo = 'follows'
+          ) T1
+          GROUP BY id_pelicula_a
+          HAVING COUNT(*) >= 2  -- es 2 y no 3 ya que la primera pelicula no se cuenta, se usa como identificador de saga
+          UNION ALL 
+          -- Devuelve las peliculas que pertenecen a la lista de sagas
+          -- siendo "saga" la primera pelicula de la saga
+          SELECT id_pelicula_b as id
+          FROM relacion_peliculas
+          WHERE id_pelicula_a IN (
+              SELECT id_pelicula_a
+              FROM relacion_peliculas
+              WHERE tipo = 'follows'
+          )
+      ) S
+      WHERE C.id = S.id and 
+          1990 <= estreno and estreno < 2000
+    ) S
+    WHERE P.id_contenido = S.id
+    GROUP BY id_actor
+    HAVING COUNT(*) = 1
+) A
+WHERE P.id = A.id_actor;
